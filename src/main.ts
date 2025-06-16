69cf71303056e4653d4ee125d7213c6aeb0c9e16
Kosta <68782786+KostaD02@users.noreply.github.com>
2025-06-16 20:29:21 +0400

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,57 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+
+const logger = new Logger('Main');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Steam Idler')
+    .setDescription('Self-hosted Steam Idler')
+    .setVersion('0.0.0')
+    .setLicense(
+      'MIT',
+      'https://github.com/kostad02/steam-idler/blob/main/LICENSE',
+    )
+    .setContact(
+      'Konstantine Datunishvili',
+      'https://konstantinedatunishvili.com',
+      'konstantine@datunishvili.ge',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
-bootstrap();
+
+bootstrap()
+  .then(() => {
+    logger.log('Server is running on port 3000');
+  })
+  .catch((error) => {
+    logger.error(error);
+  });
